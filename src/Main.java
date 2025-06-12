20782b2b33b3dc1e3c238be26589b9f765f23c15
Janell Huyck <janell.huyck@outlook.com>
2025-06-11 21:05:05 -0400

    Task 4 complete

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// require Scanner
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //define variables
+        final int LEGAL_AGE = 21;
+        int userAge;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // initialize Scanner
+        Scanner in = new Scanner(System.in);
+
+        // collect the user's age
+        System.out.print("Enter your age: ");
+
+        // verify the user has entered an integer
+        if (in.hasNextInt()) {
+            // assign user input to userAge
+            userAge = in.nextInt();
+            in.nextLine();
+            // print a message if the user is at least legal age, otherwise do nothing
+            if (userAge >= LEGAL_AGE) {
+                System.out.println("You get a wrist band!");
+            }
         }
     }
 }
